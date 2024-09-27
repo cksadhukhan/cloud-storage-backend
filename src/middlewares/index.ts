09eae565961326f4be2c36f8167091822260cf5a
Chanchal Sadhukhan <cksadhukhan@gmail.com>
2024-09-28 03:23:49 +0530

    logger setup

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,6 @@
 import { errorHandler } from "./errorHandler.middleware";
 import { authenticate } from "./authenticate.middleware";
 import * as passportMiddleware from "./passport.middleware";
+import { validateRequest } from "./validate.middleware";
 
-export { errorHandler, authenticate, passportMiddleware };
+export { errorHandler, authenticate, passportMiddleware, validateRequest };
